0094a1dcbdd82c06af39721014da3b6eca352638
Rajat Guleria <rajatguleria.gna@gmail.com>
2023-01-20 12:34:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/HomeFragment.kt
@@ -0,0 +1,96 @@
+package com.example.bottomnavigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    lateinit var mainActivity: MainActivity
+    lateinit var etName:EditText
+    lateinit var btnSave:Button
+    lateinit var spnAdd:Spinner
+    val userList= ArrayList<String>()
+
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivity=activity as MainActivity
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    @SuppressLint("MissingInflatedId", "ResourceType")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        etName=view.findViewById(R.id.etName)
+        btnSave=view.findViewById(R.id.btnSave)
+        spnAdd=view.findViewById(R.id.spnAdd)
+
+
+
+        btnSave.setOnClickListener{
+
+            val name=etName.text.toString()
+            userList.add(name)
+
+        }
+        val adapter=ArrayAdapter<String>(mainActivity,android.R.layout.simple_spinner_item,userList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spnAdd.adapter=adapter
+
+        return view
+    }
+
+
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+
+                }
+            }
+    }
+}
+
---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bottomnavigation
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+   private lateinit var navController: NavController
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController=navHostFragment.navController
+      val  btmNavigation = findViewById<BottomNavigationView>(R.id.btmNavigation)
+        setupWithNavController(btmNavigation,navController)
+
+
+    }
+
+}
